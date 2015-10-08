bb2a8e7fed62634f8c166c962200e81adec51ae9
Theunis Cilliers <ardinent@gmail.com>
2015-10-08 13:14:11 +0200

    Fixed placement of email trigger. First real funcitoning version

--- views/options.php
+++ views/options.php
@@ -1,28 +1,34 @@
 <div class="wrap">
 	<h1>Early Tickets</h2>
-	<p>By activating the option below, event tickets will be sent when the order status changes to processing, rather than at the completed state.</p>
-	<form action="options.php" method="post">
-		<?php
-			settings_fields('wc_early_tickets');							
-			$early_ticket_options = get_option('wc_early_tickets');
-		?>
-		<table border="0" cellspacing="8" cellpadding="0" class="form-table">					
-			<tr>
-				<?php $checked = ( $early_ticket_options['active'] === 'on' ) ? ' checked' : ''; ?>
-				<th>Send on processing</label></th>
-				<td>
-					<fieldset>
-						<label for="wc_early_tickets[active]">
-							<input name="wc_early_tickets[active]" type="checkbox" <?php echo $checked; ?>/> Active
-						</label>
-					</fieldset>
-				</td>
+	<?php
+		if ( !function_exists('remove_filters_for_anonymous_class') ) { ?>
+		<p>You need the plugin available at <a href="https://github.com/herewithme/wp-filters-extras" target="_blank">https://github.com/herewithme/wp-filters-extras</a> for this plugin to work.</p>
+	<?php } else { ?>
+
+		<p>By activating the option below, event tickets will be sent when the order status changes to processing, rather than at the completed state.</p>
+		<form action="options.php" method="post">
+			<?php
+				settings_fields('wc_early_tickets');							
+				$early_ticket_options = get_option('wc_early_tickets');
+			?>
+			<table border="0" cellspacing="8" cellpadding="0" class="form-table">					
+				<tr>
+					<?php $checked = ( $early_ticket_options['active'] === 'on' ) ? ' checked' : ''; ?>
+					<th>Send on processing</label></th>
+					<td>
+						<fieldset>
+							<label for="wc_early_tickets[active]">
+								<input name="wc_early_tickets[active]" type="checkbox" <?php echo $checked; ?>/> Active
+							</label>
+						</fieldset>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2">
+						<input type="submit" value="Save Changes" class="button button-primary" />
+					</td>
 			</tr>
-			<tr>
-				<td colspan="2">
-					<input type="submit" value="Save Changes" class="button button-primary" />
-				</td>
-		</tr>
-		</table>
-	</form>
+			</table>
+		</form>
+	<?php } ?>
 </div>
--- wc-events-organizer-early-tickets.php
+++ wc-events-organizer-early-tickets.php
@@ -34,24 +34,31 @@ function wc_early_ticket_load_admin_init() {
 }
 
 add_action('init', 'wc_early_tickets_make_early');
-function wc_early_tickets_make_early() {/*
-
-	NEED TO FIX THIS
+function wc_early_tickets_make_early() {
 
 	$early_ticket_options = get_option('wc_early_tickets');
 	if ( $early_ticket_options['active'] === 'on' ) {
-		$event_ticket = new Tribe__Events__Tickets__Woo__Main();
-		remove_action( 'woocommerce_order_status_completed', array($event_ticket, 'generate_tickets'), 13 );
-
-		add_action( 'woocommerce_order_status_pending_to_processing_notification', array($event_ticket, 'generate_tickets') );
-		add_action( 'woocommerce_order_status_on-hold_to_processing_notification', array($event_ticket, 'generate_tickets') );
-		add_action( 'woocommerce_order_status_failed_to_processing_notification', array($event_ticket, 'generate_tickets') );
+		if ( function_exists( 'remove_filters_for_anonymous_class') ) {
+			remove_filters_for_anonymous_class( 'woocommerce_order_status_completed','Tribe__Events__Tickets__Woo__Main', 'generate_tickets', 12 );
+			add_action( 'woocommerce_order_status_pending_to_processing_notification', 'wc_early_ticket_email', 20 );
+			add_action( 'woocommerce_order_status_on-hold_to_processing_notification', 'wc_early_ticket_email', 20 );
+			add_action( 'woocommerce_order_status_failed_to_processing_notification', 'wc_early_ticket_email', 20 );
+		}
 	}
-	*/
 }
 
 function wc_early_ticket_email() {
-
+	$post_id = false;
+	if ( isset( $_REQUEST['post_type'] ) && $_REQUEST['post_type'] == 'shop_order' ) {
+		if ( isset( $_REQUEST['post_ID'] ) ) $post_id = $_REQUEST['post_ID'];
+	}
+	if ( isset( $_REQUEST['action'] ) && $_REQUEST['action'] == 'woocommerce_mark_order_status' ) {
+		if ( isset( $_REQUEST['order_id'] ) ) $post_id = $_REQUEST['order_id'];
+	}
+	if ( $post_id ) {
+		$event_ticket = new Tribe__Events__Tickets__Woo__Main();
+		$event_ticket->generate_tickets( $post_id );
+	}
 }
 
 add_action('admin_menu', 'wc_early_ticket_load_admin_menu');
